a3bf3f6754d33ea21412b5f5d4550b8b34c041dd
matiglesias <matidxg@gmail.com>
2020-06-27 02:46:55 -0300

    Posts create service done

--- api/services/posts_service.go
+++ api/services/posts_service.go
@@ -1,6 +1,9 @@
 package services
 
 import (
+	"context"
+
+	"github.com/gauchadas/api/models"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -11,4 +14,31 @@ type PostsService struct {
 func NewPostsService(postsCollection *mongo.Collection) *PostsService {
 	return &PostsService{
 		postsCollection: postsCollection}
+}
+
+func (ps *PostsService) Create(p *models.Post) (interface{}, error) {
+	insertResult, err := ps.postsCollection.InsertOne(context.TODO(), p)
+	if err != nil {
+		return nil, err
+	}
+
+	/* 	ctx := context.TODO()
+	   	cursor, err := ps.postsCollection.Find(ctx, bson.D{{}})
+	   	if err != nil {
+	   		return nil, err
+	   	}
+	   	defer cursor.Close(ctx)
+
+	   	var results []models.Post
+	   	err = cursor.All(ctx, &results)
+	   	if err != nil {
+	   		return nil, err
+	   	}
+	   		for _, result := range results {
+	   		fmt.Println("EL BODY ES " + result.Body)
+	   		fmt.Println("EL TITULO ES " + result.Title)
+	   		fmt.Println("LA HORA ES " + result.CreatedAt.Time().String())
+	   		fmt.Println("")
+	   	} */
+	return insertResult.InsertedID, nil
 }
